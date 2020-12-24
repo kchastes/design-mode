4d22008b4c2d26fdb80ae6081c095216c412475c
kchaste <kchastesun@outlook.com>
2020-12-24 10:50:45 +0800

    context modify

--- design/decorate/DecorateTest.java
+++ design/decorate/DecorateTest.java
@@ -1,6 +1,11 @@
 package design.decorate;
 
+import java.util.List;
+
 /**
+ *
+ * @see java.util.Collections#synchronizedList(List)
+ * @see java.util.Collections#checkedList(List, Class)
  * @author KChaste Sun
  */
 public class DecorateTest {
